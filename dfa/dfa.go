dc0a0508b1a1892016df4e92a24f29a9d7ab312f
Spencer Peterson <spencerjpeterson@gmail.com>
2018-04-26 19:28:49 -0700

    export Recognize

--- dfa/dfa.go
+++ dfa/dfa.go
@@ -1,13 +1,13 @@
 package dfa
 
-// recognize returns true if the string is accepted by the DFA represented by
+// Recognize returns true if the string is accepted by the DFA represented by
 // the arguments. It is essentially the extended transition function.
-func recognize(start int, transition func(q int, a byte) int,
+func Recognize(start int, transition func(q int, a byte) int,
 	accept map[int]bool, str string) bool {
 
 	if len(str) == 0 {
 		return accept[start]
 	}
 
-	return recognize(transition(start, str[0]), transition, accept, str[1:])
+	return Recognize(transition(start, str[0]), transition, accept, str[1:])
 }
